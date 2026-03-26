bec532c97dfb482ba9e7d7d8460e8a0970a6f1c0
salasasa <174221556+salasasa@users.noreply.github.com>
2026-03-26 23:39:27 +0800

    Add JWT authentication support with Gin framework; include JWT utility functions and example configuration file.

---
+++ handler/gin/auth.go
@@ -0,0 +1,56 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/salasasa/go-publisher/util"
+)
+
+const (
+	UID_IN_TOKEN = "uid"
+	UID_IN_CTX   = "uid"
+	COOKIE_NAME  = "jwt"
+)
+
+var (
+	KeyConfig = util.InitViper("conf", "jwt", util.YAML)
+)
+
+func Auth(ctx *gin.Context) {
+	LoginUid := GetLoginUid(ctx)
+	if LoginUid <= 0 {
+		ctx.String(http.StatusUnauthorized, "请先登录")
+		ctx.Redirect(http.StatusTemporaryRedirect, "/login") //重定向到登录页面
+		ctx.Abort()
+		return
+	}
+	ctx.Set(UID_IN_CTX, LoginUid)
+}
+
+func GetLoginUid(ctx *gin.Context) int {
+	cookies := ctx.Request.Cookies()
+	for _, cookie := range cookies {
+		if cookie.Name == COOKIE_NAME {
+			return GetUidFromJwt(cookie.Value)
+		}
+	}
+
+	return 0
+}
+
+func GetUidFromJwt(token string) int {
+	_, jwtPayload, err := util.VerifyJwt(token, KeyConfig.GetString("secret"))
+	if err != nil {
+		return 0
+	}
+	userData := jwtPayload.UserDefined
+	if userData == nil {
+		return 0
+	}
+	uid, ok := userData[UID_IN_TOKEN].(int)
+	if !ok {
+		return 0
+	}
+	return uid
+}
--- handler/gin/user.go
+++ handler/gin/user.go
@@ -1,13 +1,19 @@
 package handler
 
 import (
+	"log/slog"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/salasasa/go-publisher/database/gorm"
 	"github.com/salasasa/go-publisher/handler/model"
 	"github.com/salasasa/go-publisher/util"
+)
+
+const (
+	COOKIE_LIFE = 7 * 86400
 )
 
 func RegistUser(ctx *gin.Context) {
@@ -36,23 +42,38 @@ func Login(ctx *gin.Context) {
 		return
 	}
 
-	ctx.SetCookie("uid", strconv.Itoa(userData.Id), 86400, "/", "192.168.10.110", false, true)
+	jwtHeader := util.DefautJwtHeader
+	jwtpayload := util.JwtPayload{ //payload以明文形式编码在token中，server用自己的密钥可以校验该信息是否被篡改过
+		Issue:       "news",
+		IssueAt:     time.Now().Unix(),                                //因为每次的IssueAt不同，所以每次生成的token也不同
+		Expiration:  time.Now().Add(COOKIE_LIFE * time.Second).Unix(), //7天后过期
+		UserDefined: map[string]any{UID_IN_TOKEN: userData.Id},        //用户自定义字段。如果token里包含敏感信息，请结合https使用
+	}
+	if token, err := util.GenJWT(jwtHeader, jwtpayload, KeyConfig.GetString("secret")); err != nil {
+		slog.Error("生成JWT失败", "error", err)
+		ctx.String(http.StatusInternalServerError, "token生成失败")
+		return
+	} else {
+		ctx.SetCookie(COOKIE_NAME, token, COOKIE_LIFE, "/", "192.168.10.110", false, true)
+	}
+
+	// ctx.SetCookie("uid", strconv.Itoa(userData.Id), 86400, "/", "192.168.10.110", false, true)
 }
 
 func Logout(ctx *gin.Context) {
-	ctx.SetCookie("uid", "", -1, "/", "192.168.10.110", false, true)
+	ctx.SetCookie(COOKIE_NAME, "", -1, "/", "192.168.10.110", false, true)
 }
 
 func UpdatePassword(ctx *gin.Context) {
-	req := &model.ModifyPassRequest{}
-	if err := ctx.ShouldBind(req); err != nil {
-		ctx.String(http.StatusBadRequest, util.BindErrMsg(err))
+	uid, ok := ctx.Value(UID_IN_CTX).(int)
+	if !ok {
+		ctx.String(http.StatusBadRequest, "请先登录")
 		return
 	}
 
-	uid := GetUidFromCookie(ctx)
-	if uid <= 0 {
-		ctx.String(http.StatusBadRequest, "请先登录")
+	req := &model.ModifyPassRequest{}
+	if err := ctx.ShouldBind(req); err != nil {
+		ctx.String(http.StatusBadRequest, util.BindErrMsg(err))
 		return
 	}
 
@@ -70,5 +91,10 @@ func GetUidFromCookie(ctx *gin.Context) int {
 			}
 		}
 	}
+	// for _, cookie := range ctx.Request.Cookies() {
+	// 	if cookie.Name == COOKIE_NAME {
+	// 		return GetUidFromJwt(cookie.Value)
+	// 	}
+	// }
 	return 0
 }
